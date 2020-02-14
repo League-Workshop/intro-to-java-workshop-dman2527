937970fab8ff2a56fadf69cd2033ccb98b76c854
League Student <league@iMac-14.attlocal.net>
2020-02-13 18:23:39 -0800

    Finished greater, candycrush and madlibs

---
+++ src/section3/Greater.java
@@ -0,0 +1,11 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greater {
+public static void main(String[] args) {
+	System.out.println("Luca");
+String age =JOptionPane.showInputDialog("how old are you");
+	JOptionPane.showMessageDialog(null,"I am "+age+" years old");
+}
+}
